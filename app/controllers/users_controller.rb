1dc41ef5a9208b592937fa75b0f67d0e6864e7ed
Rob Schreiber <robert.schreiber@boiseschools.org>
2015-10-02 15:48:20 -0600

    Finish log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+			log_in @user
 			flash[:success] = "Welcome to GameLogger!"
 			redirect_to @user
     else
