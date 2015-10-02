feee0db1b5edf7a41145b46d690088ea5fb74f51
Rob Schreiber <robert.schreiber@boiseschools.org>
2015-10-02 14:34:14 -0600

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-		assert_select "title", "Home | GameLogger"
+		assert_select "title", "GameLogger"
   end
 
   test "should get help" do
@@ -19,4 +19,9 @@ class StaticPagesControllerTest < ActionController::TestCase
 		assert_select "title", "About | GameLogger"
 	end
 
+	test "should get contact" do
+		get :contact
+		assert_response :success
+		assert_select "title", "Contact | GameLogger"
+	end
 end
