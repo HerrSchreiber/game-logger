b87ac738ec935dec338be1739690c1bce2caf5c7
Rob Schreiber <robert.schreiber@boiseschools.org>
2015-12-02 21:31:36 -0700

    Add jquery-turbolinks to application.js manifest in the right place

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require jquery_ujs
 //= require jquery.turbolinks
+//= require jquery_ujs
 //= require bootstrap
 //= require turbolinks
 //= require selectize
